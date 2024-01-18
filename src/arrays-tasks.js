892f3d794f3bedb215c23dd9142f264164659edc
Adam Malec <add.malec@gmail.com>
2024-01-18 10:19:12 +0300

    feat: complete createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -344,8 +344,9 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const chunkLength = Math.ceil(arr.length / chunkSize);
+  return Array.from({ length: chunkLength }, () => arr.splice(0, chunkSize));
 }
 
 /**
